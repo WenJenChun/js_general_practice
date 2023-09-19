adc595c3a4dd1622760e43116c90c80e47df4b8e
WenJenChun <daydreamsss@hotmail.com>
2023-09-19 22:25:37 +0100

    commit

---
+++ README.md
@@ -0,0 +1,4 @@
+This repo is for practicing JS
+
+此專案包含各種JS練習
+- 將流程圖轉譯為程式碼
--- all.js
+++ all.js
@@ -1,7 +1,29 @@
-let a = 1;
-let b = 2;
+//計算客戶體溫
 
-console.log(a+b);
-console.log(a,b);
+// let temp = 36;
+// if(temp>=37.5){
+//     console.log('Not allow to come in');
+// } else {
+//     console.log('welcome!');
+// }
 
-alert('omg');
+//計算成績是否及格
+// let studentScore = 30;
+// let passScore = 60;
+
+// if(studentScore>=passScore){
+//     console.log('成績及格');
+// } else {
+//     console.log('成績不及格');
+// }
+
+//計算成績平等，80以上優秀、60-79普通、59以下不及格
+let studentScore = 80;
+
+if(studentScore>=80){
+    console.log('優秀');
+} else if (studentScore>=60 && studentScore<=79){
+    console.log('普通');
+} else {
+    console.log('不及格');
+}
