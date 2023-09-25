fb463d1a01db584106b65b28398246ecf8987c2a
WenJenChun <daydreamsss@hotmail.com>
2023-09-25 23:43:13 +0100

    codewar - How good are you really?

--- codewar.js
+++ codewar.js
@@ -27,5 +27,45 @@
 
 
 //取代多個字符元素及其大小寫，以刪去母音為例
-let sentence = "This is A nice book that I like to read it";
-console.log(sentence.replace(/[aeiou]/gi, ''));
+// let sentence = "This is A nice book that I like to read it";
+// console.log(sentence.replace(/[aeiou]/gi, ''));
+
+
+//==========  How good are you really?  ==========
+// There was a test in your class and you passed it. Congratulations!
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+// Return True if you're better, else False!
+
+// Note:
+// Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
+
+function betterThanAverage(classPoints, yourPoints) {
+    //如果我的分數比平均分數高，return true，反之 false
+    //平均分數算法：(classPoints + yourPoints)/(classPoints.length + 1)
+   
+    let sum = 0;
+    let average = 0;
+    
+    //先算出所有 classPoints
+    for(i=0;  i<classPoints.length; i++){
+      sum+=classPoints[i];
+      console.log(classPoints[i]);
+    }
+    //classPoints + yourPoints然後再去算平均分數
+    sum+=yourPoints;
+    average = sum / (classPoints.length + 1 );
+    console.log(average);
+
+    //判斷並回傳結果
+    if(yourPoints >= average){
+        return true;
+    } else {
+        return false;
+    }
+    
+  }
+
+  betterThanAverage([2,4,9],10);
